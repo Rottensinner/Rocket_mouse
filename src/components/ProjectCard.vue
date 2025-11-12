97d3cfdf3608891bd6b9d4ec96f36cf59c06d92b
aleks gabryś <aleksgabrys@gmail.com>
2025-11-12 23:12:58 +0100

    adding a card rotation effect

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -1,12 +1,19 @@
 <template>
-  <div class="card-wrapper">
-    <div class="card-picture"></div>
-    <div class="card-info">
-      <p>{{ id }}. {{ name }}</p>
+  <div class="card-container">
+    <div class="card-inner">
+      <div class="card-front">
+        <div class="card-picture"></div>
+        <div class="card-info">
+          <h5>{{ id }}. {{ name }}</h5>
+        </div>
+      </div>
+
+      <div class="card-back">
+        <div class="card-info">
+          <p>{{ description }}</p>
+        </div>
+      </div>
     </div>
-    <!-- <div class="card-back-info">
-      <p>{{ description }}</p>
-    </div> -->
   </div>
 </template>
 <script setup>
@@ -17,25 +24,64 @@ defineProps({
 });
 </script>
 <style scoped>
-.card-wrapper {
+.card-container {
+  /* umożliwia to efekty 3d */
+  perspective: 1000px;
+}
+.card-inner {
+  /* ustawiamy wysokość, szerokość, dodajemy czas animacji i jej rodzaj */
+  position: relative;
   background-color: bisque;
   width: 200px;
   height: 250px;
   margin: 20px;
+  transition: transform 1s;
+  transform-style: preserve-3d;
+}
+.card-container:hover .card-inner {
+  /* po najechaniu będzie się obracać wokół osi Y */
+  transform: rotateY(180deg);
+}
+.card-front,
+.card-back {
+  position: absolute;
+  width: 200px;
+  height: 250px;
   border-radius: 5%;
+  backface-visibility: hidden;
+  overflow: hidden;
+}
+.card-front {
+  display: flex;
+  flex-direction: column;
+  align-items: baseline;
 }
 .card-picture {
   background-color: blue;
   border-radius: 5% 5% 0 0;
-
   width: 200px;
   height: 200px;
+  padding: 0;
+  margin: 0;
 }
 .card-info {
-  padding: 10px;
+  /* height: 50px; */
   color: black;
   font-size: 1.4rem;
+  /* padding-bottom: 20px; */
+  padding-left: 10px;
 }
-.card-back-info {
+.card-back {
+  /* ustawienie dla pleców karty aby były obrucone  */
+  background-color: bisque;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: #333;
+  font-size: 1.1rem;
+  transform: rotateY(180deg);
+}
+p {
+  font-size: 16px;
 }
 </style>
